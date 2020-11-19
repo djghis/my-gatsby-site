ad3053c5ca68c80874f4f3aecf4cf63899331e35
Gg <58531623+djghis@users.noreply.github.com>
2020-11-19 11:38:39 +0000

    fixed speling mistake in about me

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,7 +10,7 @@ const AboutPage = () => {
             <Head title="About me" />
             <h1>A little bit about me:</h1>
             <p>I have been working in hotel and restaurant for a long time.</p>
-            <p>And I hae no completed the Software developer course at CodeClan</p>
+            <p>And I have no completed the Software developer course at CodeClan</p>
             <p><Link to="/contact">Contact Me</Link></p> 
         </Layout>
     )
